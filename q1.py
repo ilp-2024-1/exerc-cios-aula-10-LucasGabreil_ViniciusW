2c802bb908f8c752b876b2538cd7802e4de8d933
LUCAS GABRIEL NASCIMENTO MEDEIROS <20241148060007@ifrn.local>
2024-07-10 11:42:42 -0300

    Exercício 1

---
+++ q1.py
@@ -0,0 +1,5 @@
+# Escreva um programa que exiba os valores dos números de 1 até 100. Ao término o
+# programa deverá exibir uma mensagem de encerramento informando que o programa terminou. 
+for n in range(1, 101):
+    print(n)
+print("Programa terminou.")
